517f1933756afde337c7e63da13886f6fbdcb9d1
Retro Invader <retr0.inv4der@gmail.com>
2025-09-06 21:08:28 +0330

    implementing oop

--- src/client.cpp
+++ src/client.cpp
@@ -7,49 +7,69 @@
 #include <unistd.h>
 #include <iostream>
 
-void receiveMessages(int sock){
-    char buffer[1024];
-    sockaddr_in fromAdrr;
-    socklen_t fromLen = sizeof(fromAdrr);
-    while(true){
-        int bytesReceived = recvfrom(sock, buffer, sizeof(buffer), 0 , (struct sockaddr*)&fromAdrr , &fromLen );
-        if(bytesReceived>0){
-            buffer[bytesReceived] ='\0';
-            std::cout<<"\n[SERVER]:" << buffer << std::endl <<" > " ; 
-            std::cout.flush();
 
+class UDP_Client {
+private:
+    int sockfd ;
+    struct sockaddr_in dest_addr;
+    socklen_t dest_len = sizeof(this->dest_addr) ;
+
+public:
+    UDP_Client(char ip_addr[] , int port){
+        //this function inits the socketfd and then sets the dest address 
+        //1. init the  socket 
+        if((this->sockfd = socket(AF_INET , SOCK_DGRAM , 0)) == -1){
+            perror("failed in socket creation");
+            exit(1) ; 
+        }
+
+        //2. set the dest address
+        this->dest_addr.sin_family = AF_INET ;
+        this->dest_addr.sin_port = htons(port) ;
+        this->dest_addr.sin_addr.s_addr = inet_addr(ip_addr);
+        
+    }
+
+    void receiveMessages(int sock){
+        char buffer[1024];
+        sockaddr_in fromAdrr;
+        socklen_t fromLen = sizeof(fromAdrr);
+        while(true){
+            int bytesReceived = recvfrom(sock, buffer, sizeof(buffer), 0 , (struct sockaddr*)&fromAdrr , &fromLen );
+            if(bytesReceived>0){
+                buffer[bytesReceived] ='\0';
+                std::cout<<"\n[SERVER]:" << buffer << std::endl <<" > " ; 
+                std::cout.flush();
+
+            }
         }
     }
-}
+
+    void RegisterReveiver(){
+        std::thread listener(&UDP_Client::receiveMessages ,this ,  this->sockfd);
+        listener.detach();
+    }
+
+    void start(){
+            this->RegisterReveiver();
+
+            char message[1024];
+            while (true) {
+            std::cout << " > " ; 
+            std::cin.getline(message , sizeof(message));
+            int n =  sendto(this->sockfd, message, sizeof(message), 0, (struct sockaddr*)&(this->dest_addr) ,this->dest_len );
+            if (n < 0) {
+                perror("sendto failed");
+            }
+        }
+    }
+};
+
+
 
 
 int main(){
-    //create the socket 
-    int clientfd ; 
-    if((clientfd = socket(AF_INET , SOCK_DGRAM , 0)) == -1){
-        perror("failed in socket creation");
-        exit(1) ; 
-    }
-
-
-
-    //init the dest addr
-    struct sockaddr_in dest_addr; 
-    dest_addr.sin_family = AF_INET ;
-    dest_addr.sin_port = htons(3000) ;
-    dest_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    socklen_t dest_len = sizeof(dest_addr) ;
-    char message[100] ; 
-    //main loop
-
-    //register the receiver as well
-    std::thread listener(receiveMessages , clientfd); 
-    while (true) {
-        std::cout << " > " ; 
-        std::cin.getline(message , sizeof(message));
-        int n =  sendto(clientfd, message, sizeof(message), 0, (struct sockaddr*)&dest_addr ,dest_len );
-        if (n < 0) {
-            perror("sendto failed");
-        }
-    }
+    char ip[] = "127.0.0.1";
+    UDP_Client client  = UDP_Client(ip , 3000);
+    client.start();
 }
--- src/server.cpp
+++ src/server.cpp
@@ -7,62 +7,87 @@
 #include <unistd.h>
 #include <vector>
 
-int main(){
-    //socket file descriptor creation
-    int serverfd ;
-    if((serverfd = socket(AF_INET , SOCK_DGRAM , 0)) == -1)perror("socket file descriptor creation failed");
+class UDP_Server{
+private:
+    int socket_fd;
+    std::vector<struct sockaddr> client_list;
 
-    //define the server ip and the port number
-    struct sockaddr_in address ; 
-    address.sin_port = htons(3000); //port 3000
-    address.sin_family = AF_INET ; //ipv4
-    address.sin_addr.s_addr = INADDR_ANY; //listen on all interfaces
+public:
+    UDP_Server(char ip_addr[] , int port ){
+        //this function creates a socket and binds the ip addr and the port on that socket
+        //1.create a socket
+        if((this->socket_fd = socket(AF_INET , SOCK_DGRAM , 0)) == -1)perror("socket file descriptor creation failed");
 
-    //bind the socket
-    if(bind(serverfd ,(sockaddr*)&address , sizeof(address)) ==-1){
-        perror("bind failed.");
-        close(serverfd);
-        exit(1);
-    }
-    std::cout << "listening ..." << std::endl ; 
+        //2. bind the configs to the socket
+        struct sockaddr_in address ; 
+        address.sin_port = htons(port); 
+        address.sin_family = AF_INET ; //ipv4
+         inet_pton(AF_INET, ip_addr, &(address.sin_addr));
 
-    //start getting msgs
-    int n ; 
-    char buffer[1024] ;
-    struct sockaddr client_addr ; // client socket buffer
-    socklen_t addr_len = sizeof(client_addr);
-
-    std::vector<struct sockaddr> client_list ;
-
-    while(true){
-        n = recvfrom(serverfd, buffer, 100, 0 , &client_addr , &addr_len);
-        if(client_list.size() == 0 ) client_list.push_back(client_addr);
-
-
-        bool isSameAddr = 0;
-        bool isSamePort = 0 ;
-        bool isSameClient = 0;
-        for(int i = 0 ; i<=client_list.size()-1; i++){
-            isSameAddr = (((struct sockaddr_in*)&client_addr)->sin_addr.s_addr == (((struct sockaddr_in*)&client_list[i])->sin_addr.s_addr));
-            isSamePort = (((struct sockaddr_in*)&client_addr)->sin_port == (((struct sockaddr_in*)&client_list[i])->sin_port));
-            isSameClient = isSameAddr&& isSamePort ;
-            if(isSameClient){
-                break;
-            }
+        if(bind(this->socket_fd ,(sockaddr*)&address , sizeof(address)) ==-1){
+            std::cout << ip_addr << std::endl ;
+            perror("bind failed.");
+            close(this->socket_fd);
+            exit(1);
         }
-        if(!isSameClient) client_list.push_back(client_addr);
-        std::cout<<"captured:" << buffer << std::endl ; 
+
+    }
+
+    void start(){
+        int n ; 
+        char buffer[1024] ;
+        struct sockaddr client_addr ; // client socket buffer
+        socklen_t addr_len = sizeof(client_addr);
+
+
+        while(true){
+            n = recvfrom(this->socket_fd, buffer, 100, 0 , &client_addr , &addr_len);
+            if(this->client_list.size() == 0 ) this->client_list.push_back(client_addr);
+
+
+            bool isSameAddr = 0;
+            bool isSamePort = 0 ;
+            bool isSameClient = 0;
+            for(int i = 0 ; i<=this->client_list.size()-1; i++){
+                isSameAddr = (((struct sockaddr_in*)&client_addr)->sin_addr.s_addr == (((struct sockaddr_in*)&this->client_list[i])->sin_addr.s_addr));
+                isSamePort = (((struct sockaddr_in*)&client_addr)->sin_port == (((struct sockaddr_in*)&this->client_list[i])->sin_port));
+                isSameClient = isSameAddr&& isSamePort ;
+                if(isSameClient){
+                    break;
+                }
+            }
+            if(!isSameClient) this->client_list.push_back(client_addr);
+            std::cout<<"captured:" << buffer << std::endl ; 
         
         //broadcast for all clients except for the sender
-        for(int i =0 ; i<= client_list.size()-1 ; i++){
-            isSameAddr = (((struct sockaddr_in*)&client_addr)->sin_addr.s_addr == (((struct sockaddr_in*)&client_list[i])->sin_addr.s_addr));
-            isSamePort = (((struct sockaddr_in*)&client_addr)->sin_port == (((struct sockaddr_in*)&client_list[i])->sin_port));
-            isSameClient = isSameAddr&& isSamePort ;
-            if(!isSameClient){
-                sendto(serverfd, buffer, 100, 0 ,(struct sockaddr* ) &client_list[i] , sizeof(client_list[i]));
+            for(int i =0 ; i<= this->client_list.size()-1 ; i++){
+                isSameAddr = (((struct sockaddr_in*)&client_addr)->sin_addr.s_addr == (((struct sockaddr_in*)&this->client_list[i])->sin_addr.s_addr));
+                isSamePort = (((struct sockaddr_in*)&client_addr)->sin_port == (((struct sockaddr_in*)&this->client_list[i])->sin_port));
+                isSameClient = isSameAddr&& isSamePort ;
+                if(!isSameClient){
+                    sendto(this->socket_fd, buffer, 100, 0 ,(struct sockaddr* ) &this->client_list[i] , sizeof(this->client_list[i]));
                 
+                }
             }
-        }
         
-    }   
+        }   
+
+
+    }
+
+
+
+
+
+};
+
+
+
+
+
+int main(){
+    //socket file descriptor creation
+    char ip[] = "127.0.0.1";
+   UDP_Server server = UDP_Server( ip, 3000);
+   server.start();
 }
